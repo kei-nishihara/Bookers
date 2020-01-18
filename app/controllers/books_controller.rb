c19b0b763bb141ab4c3086bdf15a917921e5025a
西原　慧 <nishiharakei@nishiharatoshinoMacBook-Pro.local>
2020-01-18 21:46:53 +0900

    bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,10 +37,14 @@ def edit
 end
 
 def update
-	book = Book.find(params[:id])
-	book.update(book_params)
-      	flash[:notice] = "Book was successfully update."
-	redirect_to book_path(book.id)
+	
+	@book = Book.find(params[:id])
+	if @book.update(book_params)
+		flash[:notice] = "Book was successfully update."
+		redirect_to book_path(@book.id)
+	else
+		render :edit
+	end
 end
 
 def destroy
@@ -55,4 +59,3 @@ def book_params
 	params.require(:book).permit(:title, :category, :body)
 end
 end
-
