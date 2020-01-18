352dcf858acb7beaf01b08a46c411a00bc4b7e6d
西原　慧 <nishiharakei@nishiharatoshinoMacBook-Pro.local>
2020-01-18 18:22:21 +0900

    bookers

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'root' => 'books#root'
-  get 'books/:id/edit' => 'books#edit',as: 'edit_books'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
+  root 'books#root'
 
   resources:books
   resources:users,only:[:index, :edit, :update]
